7e410d692d07657f9666a3ab7ae21634b01e56ac
kenjiclayton <52931772+kenjiclayton@users.noreply.github.com>
2024-08-20 20:03:37 +0800

    update
    cursor

---
+++ main.js
@@ -0,0 +1,20 @@
+// Site wide cursor
+const site_wide_cursor = document.querySelector('.custom-cursor.site-wide');
+
+document.addEventListener('mouseenter', () => {
+    site_wide_cursor.style.display = 'block';
+});
+
+document.addEventListener('mouseleave', () => {
+    site_wide_cursor.style.display = 'none';
+});
+
+document.addEventListener('mousemove', TrackCursor);
+
+document.addEventListener('mousedown', () => site_wide_cursor.classList.add('active'));
+
+document.addEventListener('mouseup', () => site_wide_cursor.classList.remove('active'));
+
+function TrackCursor(evt) {
+    site_wide_cursor.style.transform = `translate(${evt.clientX}px, ${evt.clientY}px)`;
+}
